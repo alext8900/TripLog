2f4180c7b3fef17c7ecdbf0162badd919f052d1b
Alexander Thompson <53663334+alext8900@users.noreply.github.com>
2021-03-24 14:20:35 -0500

    Initial Commit

---
+++ Logbook/LogbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogbookApp.swift
+//  Logbook
+//
+//  Created by Alex Thompson on 3/24/21.
+//
+
+import SwiftUI
+
+@main
+struct LogbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
