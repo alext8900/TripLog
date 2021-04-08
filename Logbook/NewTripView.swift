2459999ceff23dbad0523911ac989e41ebe60e8c
Alexander Thompson <53663334+alext8900@users.noreply.github.com>
2021-04-08 00:30:26 -0500

    Working on newTripView
    Added a vstack with image and user name, added some user input places where users could say their starting location and the date. Added a custom divider

--- Logbook/NewTripView.swift
+++ Logbook/NewTripView.swift
@@ -12,10 +12,80 @@ struct NewTripView: View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [Color.red, Color.yellow]), startPoint: .bottomTrailing, endPoint: .top)
                 .ignoresSafeArea()
-            VStack {
+            VStack(spacing: 0) {
+                Image("truck")
+                    .resizable()
+                    .renderingMode(.original)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 125, height: 135)
+                    .padding()
                 
+                Text("Charles Thompson")
+                    .font(.system(size: 35, weight: .semibold))
+                    .padding(20)
+                
+                HStack {
+                    Text("From :")
+                        .font(.system(size: 23, weight: .regular))
+                        .padding()
+                    Spacer()
+                }
+                ExtendedDivider()
+
+                HStack {
+                    UserInputView(imageName: "location", userInputName: "Starting Location")
+                }
+                ExtendedDivider()
+                
+                HStack {
+                    Image(systemName: "calendar")
+                        .resizable()
+                        .renderingMode(.original)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 26, height: 27)
+                        .padding(10)
+                    
+                    
+                    Text("\(Date().addingTimeInterval(600), style: .date) ,")
+                        .font(.system(size: 20, weight: .light))
+                    
+                    Text(Date().addingTimeInterval(0), style: .time)
+                        .font(.system(size: 20, weight: .light))
+                    
+                    Spacer()
+                }
+                ExtendedDivider()
+                Spacer()
+                HStack {
+                    Text("From :")
+                        .font(.system(size: 23, weight: .regular))
+                        .padding()
+                    Spacer()
+                }
             }
         }
+    }
+}
+
+struct UserInputView: View {
+    var imageName: String
+    var userInputName: String
+    
+    var body: some View {
+        
+            Image(systemName: imageName)
+                .resizable()
+                .renderingMode(.original)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26, height: 27)
+                .padding(10)
+            
+            
+            Text(userInputName)
+                .font(.system(size: 20, weight: .light))
+            
+            Spacer()
+            
     }
 }
 
